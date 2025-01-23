cc8dce999528f56ce078e66813d35e86cb2c15af
Pund47 <ilin_yuriy@mail.ru>
2025-01-23 11:52:03 +0300

    1

--- Bot/Hadlers/registration.py
+++ Bot/Hadlers/registration.py
@@ -1,8 +1,8 @@
 from  Bot.Keybords.registration import *
 from  Bot.Keybords.welcome import *
 
-from aiogram import types, Dispatcher
-from aiogram.dispatcher.filters.state import State
+from aiogram import Dispatcher
+
 
 from Bot.Models.user import User
 
@@ -11,24 +11,25 @@ async def start_registration(message):
     await message.answer(f"Welcome, {message.from_user.username}",reply_markup=registration_kb)
 
 #Подключение к базе на проверку пользователя!
-async def Autorisation(call):
+async def autorisation_1(call):
     await call.message.answer()          #!!!!!!
     await call.answer()
 
 #Подключение к базе на Добавление нового пользователя!
-async def Registration_new_user(call,state):
+async def registration_new_user(call,state):
     await call.message.answer("Введите имя:")
     await state.set_state("user_name")
+    await call.answer()
 
 async def registration_new_user_password(message,state):
     await state.update_data(username = message.text)
-    data = await state.get_data()
+#    data = await state.get_data()
     await message.answer(f"Введите пароль:")
     await state.set_state("set_password")
 
 async def registration_new_user_age(message,state):
     await state.update_data(password=message.text)
-    data = await state.get_data()
+#    data = await state.get_data()
     await message.answer(f"Введите ваш возраст:")
     await state.set_state("set_age")
 
@@ -38,15 +39,16 @@ async def create_new_user(message,state):
     await User.create(user_id= message.from_user.id,**data)
     await state.finish()
 
-async def Back_to_start(call,state):
+async def back_to_start(call):
     await call.message.answer(f"Welcome",reply_markup=start_kb)
+    await call.answer()
 
 
 
 def register_handlers_registration(dp:Dispatcher):
-    dp.register_callback_query_handler(Autorisation, text='Authorisation')
-    dp.register_callback_query_handler(Registration_new_user, text='Registration')
-    dp.register_callback_query_handler(Back_to_start, text="back_to_start")
+    dp.register_callback_query_handler(autorisation_1, text='Authorisation')
+    dp.register_callback_query_handler(registration_new_user, text='Registration')
+    dp.register_callback_query_handler(back_to_start, text="back_to_start")
     dp.register_message_handler(registration_new_user_password,state= "user_name")
     dp.register_message_handler(registration_new_user_age, state= "set_password")
     dp.register_message_handler(create_new_user, state= "set_age")
